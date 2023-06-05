6ca07b00c8570d2c022383fc3738a3bbc21a2258
MAKMOHA <MAKMOHA@MSS-L-MAKMOHA>
2023-06-05 14:49:56 +0800

    Add project files.

---
+++ test_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("test_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("test_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("59FEE0EA-5296-448D-B7B7-A964CCD0A230")]
+[assembly: AssemblyVersion("1.0.0.0")]
